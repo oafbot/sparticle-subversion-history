c5e8574bd61f91726759b8812255b917b43a6ba4
laika <laika@ba2ecddf-2a39-4db2-bd05-0902b79c7f83>
2012-03-25 23:00:15 +0000

    pagination changes, comments module, application config file, lots of other changes…

---
+++ folio/application/control/Comment_Controller.php
@@ -0,0 +1,41 @@
+<?php
+class FOLIO_Comment_Controller extends LAIKA_Abstract_Page_Controller {
+
+//-------------------------------------------------------------------
+//	PROPERTIES
+//-------------------------------------------------------------------
+
+    protected static $instance;
+    protected        $parameters;
+    public    static $access_level = 'PUBLIC';
+    public    static $access_group = 'USER';
+
+//-------------------------------------------------------------------
+//	METHODS
+//-------------------------------------------------------------------
+	
+	public function default_action(){ $this->pagination(); }
+	
+	public function add(){
+	   $comment = new FOLIO_Comment();
+	   $user = LAIKA_User::load($_REQUEST['user']);
+	   
+	   $comment->user = $_REQUEST['user'];
+	   $comment->parent_type = $_REQUEST['parent_type'];
+	   $comment->parent_id   = $_REQUEST['parent_id'];
+	   $comment->comment     = $_REQUEST['comment'];
+	   $comment->user_link   = '<a href="'.HTTP_ROOT.'/user/'.$user->username.'" >'.$user->username.'</a>';
+	   
+	      
+	   FOLIO_Comment::add($comment);
+	   
+	   $json = array('new_comment'=>$comment->comment,'user_link'=>$comment->user_link);
+	   echo json_encode($json);
+	}
+	
+	public function pagination(){
+	   $page = $this->parameters['p'];
+	   $json = array('page'=>$page);
+	   echo json_encode($json);
+	}
+}
